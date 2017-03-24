d147a79e1106e9f918ae97550ab6b2ce9b00e7c3
1103137208 <1103137208@gm.kuas.edu.tw>
2017-03-24 15:19:53 +0800

    123

---
+++ WebApplication3/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+        public ActionResult Index()
+        {
+            Models.OrderService OrderService=new Models.OrderService();
+            var order=OrderService.GetOrderById("111");
+            ViewBag.CustId=order.CustId;
+            ViewBag.CustName=order.CustName;
+            return View();
+        }
+        public ActionResult InsertOrder()
+        {
+            return View();
+        }
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order)
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
+
+        [HttpGet()]
+        public JsonResult TestJson()
+        {
+            var result = new Models.Order();
+            result.CustId = "GSS";
+            result.CustName = "瑞陽";
+
+            return this.Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
